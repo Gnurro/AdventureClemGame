c2588f04ceec9f72771ec2c5696c522bdced0f5e
Gnurro <knawurzelkopp@hotmail.de>
2024-11-21 19:33:09 +0100

    Prototype conversion of v1 basic actions to new PDDL-based format in basic_actions_v2.json, including new feedback; Readable basic action PDDL txt files; Add 'receptacle' trait to supports and containers in home_entities_v2.json; Work on resolution-only IF interpreter for testing new action format in pddl_experiments.py

--- adventuregame/pddl_experiments.py
+++ adventuregame/pddl_experiments.py
@@ -1,5 +1,11 @@
+import json
+import os
+from copy import deepcopy
+
 import lark
 from lark import Lark, Transformer
+
+from adv_util import fact_str_to_tuple, fact_tuple_to_str
 
 
 with open("pddl_actions.lark") as grammar_file:
@@ -7,7 +13,7 @@ with open("pddl_actions.lark") as grammar_file:
 
 # print(action_grammar)
 
-parser = Lark(action_grammar, start="action")
+action_parser = Lark(action_grammar, start="action")
 
 # test_pddl = "test_pddl_actions.txt"
 test_pddl = "test_pddl_actions1.txt"
@@ -16,7 +22,7 @@ with open(test_pddl) as action_file:
     action_raw = action_file.read()
 
 
-parsed_action = parser.parse(action_raw)
+parsed_action = action_parser.parse(action_raw)
 
 # print(parsed_action)
 
@@ -30,7 +36,7 @@ class PDDLActionTransformer(Transformer):
         # print("action cont:", content)
 
         # action_def_dict = {'action_name': content[1].value, 'content': content[3:]}
-        action_def_dict = {'action_name': content[1].value}
+        action_def_dict = {'action_name': content[1].value.lower()}
 
         for cont in content:
             # print(type(cont))
@@ -198,9 +204,9 @@ class PDDLActionTransformer(Transformer):
         return {'variable': content[0].value}
 
 
-pddl_transformer = PDDLActionTransformer()
+action_def_transformer = PDDLActionTransformer()
 
-action_def = pddl_transformer.transform(parsed_action)
+action_def = action_def_transformer.transform(parsed_action)
 
 # print(action_def)
 
@@ -212,9 +218,34 @@ def pretty_action(action: dict):
 
 # PROCESSING
 
+# action_def_source = "resources/definitions/basic_actions_v2.json"
 
 class TestIF:
-    def __init__(self, action_raw: str):
+    def __init__(self, game_instance: dict):
+        # game instance is the instance data as passed by the GameMaster class
+        self.game_instance: dict = game_instance
+        # surface strings (repr_str here) to spaceless internal identifiers:
+        self.repr_str_to_type_dict: dict = dict()
+
+        self.entity_types = dict()
+        self.initialize_entity_types()
+
+        self.room_types = dict()
+        self.initialize_room_types()
+
+        self.action_types = dict()
+        self.initialize_action_types()
+
+        self.world_state: set = set()
+        self.world_state_history: list = list()
+        self.goal_state: set = set()
+        self.goals_achieved: set = set()
+        self.initialize_states_from_strings()
+
+        # self.initialize_action_parsing(print_lark_grammar=verbose)
+
+        # first prototype
+        """
         self.action_types = dict()
 
         self.action_parser = Lark(action_grammar, start="action")
@@ -228,14 +259,183 @@ class TestIF:
             if not action_attribute == 'action_name':
                 self.action_types[action_def['action_name'].lower()][action_attribute] = action_def[action_attribute]
 
-        print(self.action_types)
+        # print(self.action_types)
+        """
+
+    def load_json(self, json_file_path):
+        with open(f"{json_file_path}.json", 'r', encoding='utf-8') as json_file:
+            json_content = json.load(json_file)
+        return json_content
+
+    def initialize_entity_types(self):
+        """
+        Load and process entity types in this adventure.
+        Definitions are loaded from external files.
+        """
+        # load entity type definitions in game instance:
+        entity_definitions: list = list()
+        for entity_def_source in self.game_instance["entity_definitions"]:
+            entities_file = self.load_json(f"resources{os.sep}definitions{os.sep}{entity_def_source[:-5]}")
+            entity_definitions += entities_file
+
+        for entity_definition in entity_definitions:
+            self.entity_types[entity_definition['type_name']] = dict()
+            for entity_attribute in entity_definition:
+                if entity_attribute == 'type_name':
+                    # assign surface strings:
+                    self.repr_str_to_type_dict[entity_definition['repr_str']] = entity_definition[entity_attribute]
+                else:
+                    # get all other attributes:
+                    self.entity_types[entity_definition['type_name']][entity_attribute] = entity_definition[
+                        entity_attribute]
+
+    def initialize_room_types(self):
+        """
+        Load and process room types in this adventure.
+        Definitions are loaded from external files.
+        """
+        # load room type definitions in game instance:
+        room_definitions: list = list()
+        for room_def_source in self.game_instance["room_definitions"]:
+            rooms_file = self.load_json(f"resources{os.sep}definitions{os.sep}{room_def_source[:-5]}")
+            room_definitions += rooms_file
+
+        for room_definition in room_definitions:
+            self.room_types[room_definition['type_name']] = dict()
+            for room_attribute in room_definition:
+                if room_attribute == 'type_name':
+                    # assign surface strings:
+                    self.repr_str_to_type_dict[room_definition['repr_str']] = room_definition[room_attribute]
+                else:
+                    # get all other attributes:
+                    self.room_types[room_definition['type_name']][room_attribute] = room_definition[
+                        room_attribute]
+
+    def initialize_action_types(self):
+        """
+        Load and process action types in this adventure.
+        Definitions are loaded from external files.
+        """
+
+        # TODO: read and parse PDDL action representation
+
+        # load action type definitions in game instance:
+        action_definitions: list = list()
+        for action_def_source in self.game_instance["action_definitions"]:
+            actions_file = self.load_json(f"resources{os.sep}definitions{os.sep}{action_def_source[:-5]}")
+            action_definitions += actions_file
+
+        for action_definition in action_definitions:
+            self.action_types[action_definition['type_name']] = dict()
+            # get all action attributes:
+            for action_attribute in action_definition:
+                if not action_attribute == 'type_name':
+                    self.action_types[action_definition['type_name']][action_attribute] = action_definition[
+                        action_attribute]
+
+        for action_type in self.action_types:
+            cur_action_type = self.action_types[action_type]
+
+            if 'pddl' in cur_action_type:
+                print(cur_action_type['pddl'])
+                parsed_action_pddl = action_parser.parse(cur_action_type['pddl'])
+                processed_action_pddl = action_def_transformer.transform(parsed_action_pddl)
+                print(processed_action_pddl)
+
+            # convert fact to change from string to tuple:
+            # cur_action_type['object_post_state'] = fact_str_to_tuple(cur_action_type['object_post_state'])
+
+    def initialize_states_from_strings(self):
+        """
+        Initialize the world state set from instance data.
+        Converts List[Str] world state format into Set[Tuple].
+        """
+        for fact_string in self.game_instance['initial_state']:
+            self.world_state.add(fact_str_to_tuple(fact_string))
+
+        # NOTE: The following world state augmentations are left in here to make manual adventure creation/modification
+        # convenient. Initial adventure world states generated with the clingo adventure generator already cover these
+        # augmentations.
+
+        # facts to add are gathered in a set to prevent duplicates
+        facts_to_add = set()
+
+        # add trait facts for objects:
+        for fact in self.world_state:
+            if fact[0] == 'type':
+                # add trait facts by entity type:
+                # print(fact)
+                if 'traits' in self.entity_types[fact[2]]:
+                    type_traits: list = self.entity_types[fact[2]]['traits']
+                    for type_trait in type_traits:
+                        facts_to_add.add((type_trait, fact[1]))
+
+        # add floors to rooms:
+        for fact in self.world_state:
+            if fact[0] == 'room':
+                facts_to_add.add(('type', f'{fact[1]}floor', 'floor'))
+                # add floor:
+                facts_to_add.add(('at', f'{fact[1]}floor', fact[1]))
+
+        self.world_state = self.world_state.union(facts_to_add)
+
+        # dict with the type for each entity instance in the adventure:
+        self.inst_to_type_dict = dict()
+        # get entity instance types from world state:
+        for fact in self.world_state:
+            # entity instance to entity type mapping:
+            if fact[0] == 'type':
+                self.inst_to_type_dict[fact[1]] = fact[2]
+
+        # dict with the type for each room instance in the adventure:
+        self.room_to_type_dict = dict()
+        # get room instance types from world state:
+        for fact in self.world_state:
+            # room instance to room type mapping:
+            if fact[0] == 'room':
+                self.room_to_type_dict[fact[1]] = fact[2]
+
+        # put 'supported' items on the floor if they are not 'in' or 'on':
+        for fact in self.world_state:
+            if fact[1] in self.inst_to_type_dict:
+                if self.inst_to_type_dict[fact[1]] in self.entity_types:
+                    pass
+            if fact[0] == 'at' and ('needs_support', fact[1]) in self.world_state:
+                currently_supported = False
+                for state_pred2 in self.world_state:
+                    if state_pred2[0] == 'on' and state_pred2[1] == fact[1]:
+                        currently_supported = True
+                        break
+                    if state_pred2[0] == 'in' and state_pred2[1] == fact[1]:
+                        currently_supported = True
+                        break
+                if not currently_supported:
+                    facts_to_add.add(('on', fact[1], f'{fact[2]}floor'))
+
+        self.world_state = self.world_state.union(facts_to_add)
+        # add initial world state to world state history:
+        self.world_state_history.append(deepcopy(self.world_state))
+        """
+        # get goal state fact set:
+        for fact_string in self.game_instance['goal_state']:
+            self.goal_state.add(fact_str_to_tuple(fact_string))
+        """
 
 
-test_interpreter = TestIF(action_raw)
+
+test_instance = {"initial_state":
+                    {"type(player1,player)", "at(player1,hallway1)",
+                    "room(hallway1,hallway)", "room(kitchen1,kitchen)",
+                    "exit(kitchen1,hallway1)", "exit(hallway1,kitchen1)",
+                    "type(sandwich1,sandwich)", "takeable(sandwich1)",
+                    "in(sandwich1,inventory)"},
+                "action_definitions": ["basic_actions_v2.json"],
+                "room_definitions": ["home_rooms.json"],
+                "entity_definitions": ["home_entities.json"]}
+
+test_interpreter = TestIF(test_instance)
 
 
 action_input = {'type': "go", 'arg1': "kitchen"}
 
-starting_state = {"at(player1,hallway1)", "room(hallway1,hallway)", "room(kitchen1,kitchen)", "exit(kitchen1,hallway1)",
-                  "exit(hallway1,kitchen1)"}
 
